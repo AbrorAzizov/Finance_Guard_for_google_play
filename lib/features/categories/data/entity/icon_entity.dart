21ae943b0e676781772d51dcbff2687bec3dcb62
AbrorAzizov <houston.070224@gmail.com>
2025-10-27 22:16:59 +0500

    icons list fixed

--- lib/features/categories/data/entity/icon_entity.dart
+++ lib/features/categories/data/entity/icon_entity.dart
@@ -1,13 +1,29 @@
+import 'package:flutter/material.dart';
+
 class IconEntity {
+  final String id;
   final int iconCodePoint;
   final String? iconFontFamily;
   final int color;
-  final String id;
 
-  IconEntity(
-      {
-        required this.id,
-        required this.iconCodePoint,
-      required this.iconFontFamily,
-      required this.color});
+  IconEntity({
+    required this.id,
+    required this.iconCodePoint,
+    required this.iconFontFamily,
+    required this.color,
+  });
+
+
+  factory IconEntity.withColor({
+    required String id,
+    required IconData icon,
+    required Color color,
+  }) {
+    return IconEntity(
+      id: id,
+      iconCodePoint: icon.codePoint,
+      iconFontFamily: icon.fontFamily,
+      color: color.value, // automatic conversion
+    );
+  }
 }
